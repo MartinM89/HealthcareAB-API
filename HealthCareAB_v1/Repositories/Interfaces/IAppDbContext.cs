cb1289fff44a4f69931e99c6b27cb9e5e76a1f23
Martin <saadv89@gmail.com>
2026-01-08 10:15:55 +0100

    Removed 'Get' prefix from 'CaregiverDailySchedules' and 'CaregiverStatuses'

--- HealthCareAB_v1/Repositories/Interfaces/IAppDbContext.cs
+++ HealthCareAB_v1/Repositories/Interfaces/IAppDbContext.cs
@@ -9,8 +9,8 @@ namespace HealthCareAB_v1.Repositories.Interfaces
         public DbSet<Booking> Bookings { get; set; }
         public DbSet<TimeSlot> TimeSlots { get; set; }
         public DbSet<Caregiver> Caregivers { get; set; }
-        public DbSet<CaregiverDailySchedule> GetCaregiverDailySchedules { get; set; }
-        public DbSet<CaregiverStatus> GetCaregiverStatuses { get; set; }
+        public DbSet<CaregiverDailySchedule> CaregiverDailySchedules { get; set; }
+        public DbSet<CaregiverStatus> CaregiverStatuses { get; set; }
 
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
     }
