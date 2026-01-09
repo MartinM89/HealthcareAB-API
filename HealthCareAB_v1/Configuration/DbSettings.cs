930745d7c9f91ff72cf827aead5022e4780acc90
Dzengiz Prentic <dzengiz.prentic@hotmail.com>
2026-01-09 11:47:20 +0100

    Implemented Options pattern for Database settings.

--- HealthCareAB_v1/Configuration/DbSettings.cs
+++ HealthCareAB_v1/Configuration/DbSettings.cs
@@ -3,5 +3,5 @@ namespace HealthCareAB_v1.Configuration;
 public class DbSettings
 {
     public const string SectionName = "DbConnectionStrings";
-    public string DbConnectionString { get; set; } = string.Empty;
+    public string ConnectionString { get; set; } = string.Empty;
 }
