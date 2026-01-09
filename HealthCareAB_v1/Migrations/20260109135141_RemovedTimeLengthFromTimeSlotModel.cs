ffeffc0e6ae7c0d707eee764e626a76ff36755f3
Martin <saadv89@gmail.com>
2026-01-09 14:55:59 +0100

    Updated TimeSlot model schema in DB

---
+++ HealthCareAB_v1/Migrations/20260109135141_RemovedTimeLengthFromTimeSlotModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthCareAB_v1.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedTimeLengthFromTimeSlotModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TimeLength",
+                table: "TimeSlots");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "TimeLength",
+                table: "TimeSlots",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
