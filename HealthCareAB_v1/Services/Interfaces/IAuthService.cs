101ac9a27f4e36dc5156741206784cd4cf27f1d8
Martin <saadv89@gmail.com>
2026-01-12 15:53:26 +0100

    Make a new register service for caregivers

--- HealthCareAB_v1/Services/Interfaces/IAuthService.cs
+++ HealthCareAB_v1/Services/Interfaces/IAuthService.cs
@@ -1,10 +1,12 @@
 ﻿using HealthCareAB_v1.DTOs;
+using HealthCareAB_v1.DTOs.Auth;
 
 namespace HealthCareAB_v1.Services.Interfaces
 {
     public interface IAuthService
     {
-        Task<AuthResponseDto> RegisterAsync(RegisterDto registerDto);
+        Task<AuthResponseDto> RegisterAsync(RegisterPatientDto registerDto);
+        Task<AuthResponseDto> RegisterCaregiverAsync(RegisterCaregiverDto registerDto);
         Task<(AuthResponseDto response, string? token)> LoginAsync(LoginDto loginDto);
         CookieOptions GetJwtCookieOptions();
         CookieOptions GetClearCookieOptions();
