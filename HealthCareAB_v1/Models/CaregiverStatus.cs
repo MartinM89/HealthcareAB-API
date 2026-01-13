023e75920442b0c1748d7f729e41fd0c89028d5f
Dzengiz <dzengiz.prentic@hotmail.com>
2026-01-13 21:35:09 +0100

    Fixed ambigious code for CaregiverStatus model.

--- HealthCareAB_v1/Models/CaregiverStatus.cs
+++ HealthCareAB_v1/Models/CaregiverStatus.cs
@@ -2,17 +2,8 @@ namespace HealthCareAB_v1.Models;
 
 public class CaregiverStatus
 {
-    // public Guid Id { get; set; }
+    public Guid Id { get; set; }
+    public string Status { get; set; } = string.Empty;
 
-    // public string Status { get; set; } = "UNAVAILABLE";
-    public const string Available = "AVAILABLE";
-    public const string Unavailable = "UNAVAILABLE";
-
-    public static bool IsValidStatus(string status)
-    {
-        return status == Available || status == Unavailable;
-    }
-
-    // public ICollection<CaregiverDailySchedule> CaregiverDailySchedules { get; set; } = [];
-    public ICollection<string> CaregiverDailySchedules { get; set; } = [Available, Unavailable];
+    public ICollection<CaregiverDailySchedule> CaregiverDailySchedules { get; set; } = [];
 }
