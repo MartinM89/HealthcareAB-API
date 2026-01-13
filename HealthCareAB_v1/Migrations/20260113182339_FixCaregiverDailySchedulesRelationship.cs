a26832889d1ff47ccb9c4b859fd9c06ef2ada5e0
Dzengiz <dzengiz.prentic@hotmail.com>
2026-01-13 21:33:04 +0100

    Added a new migration to fix caregiver daily schedule relationship.

---
+++ HealthCareAB_v1/Migrations/20260113182339_FixCaregiverDailySchedulesRelationship.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthCareAB_v1.Migrations;
+
+/// <inheritdoc />
+public partial class FixCaregiverDailySchedulesRelationship : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropForeignKey(
+            name: "FK_Bookings_Patients_PatientUserId",
+            table: "Bookings"
+        );
+
+        migrationBuilder.RenameColumn(
+            name: "PatientUserId",
+            table: "Bookings",
+            newName: "PatientId"
+        );
+
+        migrationBuilder.RenameIndex(
+            name: "IX_Bookings_PatientUserId",
+            table: "Bookings",
+            newName: "IX_Bookings_PatientId"
+        );
+
+        migrationBuilder.AddForeignKey(
+            name: "FK_Bookings_Patients_PatientId",
+            table: "Bookings",
+            column: "PatientId",
+            principalTable: "Patients",
+            principalColumn: "UserId",
+            onDelete: ReferentialAction.Cascade
+        );
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropForeignKey(name: "FK_Bookings_Patients_PatientId", table: "Bookings");
+
+        migrationBuilder.RenameColumn(
+            name: "PatientId",
+            table: "Bookings",
+            newName: "PatientUserId"
+        );
+
+        migrationBuilder.RenameIndex(
+            name: "IX_Bookings_PatientId",
+            table: "Bookings",
+            newName: "IX_Bookings_PatientUserId"
+        );
+
+        migrationBuilder.AddForeignKey(
+            name: "FK_Bookings_Patients_PatientUserId",
+            table: "Bookings",
+            column: "PatientUserId",
+            principalTable: "Patients",
+            principalColumn: "UserId",
+            onDelete: ReferentialAction.Cascade
+        );
+    }
+}
