dbe443823c8a3809db219feac06c4d6dcfdfc71f
Dzengiz <dzengiz.prentic@hotmail.com>
2026-01-13 21:13:40 +0100

    Removed create caregiver endpoint which was created for testing.

--- HealthCareAB_v1/Controllers/CaregiverController.cs
+++ HealthCareAB_v1/Controllers/CaregiverController.cs
@@ -1,5 +1,4 @@
 using System.Security.Claims;
-using HealthCareAB_v1.DTOs.Auth;
 using HealthCareAB_v1.Models;
 using HealthCareAB_v1.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
@@ -9,23 +8,13 @@ namespace HealthCareAB_v1.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class CaregiverController(ICaregiverService caregiverService, IAuthService authService)
-    : ControllerBase
+public class CaregiverController(ICaregiverService caregiverService) : ControllerBase
 {
     private readonly ICaregiverService _caregiverService = caregiverService; //Change to Caregiver service later.
-    private readonly IAuthService _authService = authService; //Change to Caregiver service later.
 
     private string? GetUserId()
     {
         return User?.FindFirstValue(ClaimTypes.NameIdentifier);
-    }
-
-    [HttpPost("Create/Caregiver")]
-    public async Task<IActionResult> CreateCaregiverAsync(RegisterCaregiverDto registerDto)
-    {
-        var createdCaregiver = await _authService.RegisterCaregiverAsync(registerDto);
-
-        return Ok(createdCaregiver);
     }
 
     [HttpGet("GetUpcomingSchedule")]
