4c0a530f4cba80631f333fcbb1f193c81d9c211a
Jimmy Johansson <jimmyjohansson@outlook.com>
2026-01-13 13:45:39 +0100

    ädrat till record från class

--- HealthCareAB_v1/DTOs/Caregiver/CreateCaregiverDailyScheduleDto.cs
+++ HealthCareAB_v1/DTOs/Caregiver/CreateCaregiverDailyScheduleDto.cs
@@ -1,13 +1,9 @@
 namespace HealthCareAB_v1.DTOs.Caregiver;
 
-public sealed class CreateCaregiverDailyScheduleDto
-{
-    public DateTimeOffset StartDate { get; init; }
-    public DateTimeOffset EndDate { get; init; }
-
-    public TimeOnly WorkingStartTime { get; init; }
-    public TimeOnly WorkingEndTime { get; init; }
-
-    public Guid CaregiverId { get; init; }
-    public Guid CaregiverStatusId { get; init; }
-}
+public record CreateCaregiverDailyScheduleDto(
+    DateTime StartTime,
+    DateTime EndTime,
+    Guid CaregiverId,
+    Guid CaregiverStatusId,
+    Guid BookingId
+);
