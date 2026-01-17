def60894b38a71d042635d68ad7b63b5de485e6e
Martin <saadv89@gmail.com>
2026-01-17 13:47:05 +0100

    Remove roles from patient registration

--- HealthCareAB_v1/DTOs/Auth/RegisterDto.cs
+++ HealthCareAB_v1/DTOs/Auth/RegisterDto.cs
@@ -15,14 +15,6 @@ public class RegisterPatientDto
     [Required(ErrorMessage = "Password is required")]
     [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters")]
     public required string Password { get; set; }
-
-    /// // <summary>
-    /// Optional roles for the new user.
-    /// Note: Admin role can be assigned manually through Swagger. This is ok in dev, in the future this should
-    /// be changed to a more solid sulotion. For now you can leave it as it is if you want.
-    /// Non-admin requests with Admin role will be ignored (defaults to User).
-    /// </summary>
-    public List<string> Roles { get; set; } = [];
 }
 
 public class RegisterCaregiverDto
