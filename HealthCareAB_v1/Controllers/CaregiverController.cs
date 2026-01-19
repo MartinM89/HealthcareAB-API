43086b39dac170109d11e9f1189e8b7b5a702249
Dzengiz <dzengiz.prentic@hotmail.com>
2026-01-19 15:39:57 +0100

    Added try/catch handling in caregiver controller

--- HealthCareAB_v1/Controllers/CaregiverController.cs
+++ HealthCareAB_v1/Controllers/CaregiverController.cs
@@ -1,4 +1,6 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Security.Claims;
+using HealthCareAB_v1.Exceptions;
 using HealthCareAB_v1.Models;
 using HealthCareAB_v1.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
@@ -6,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HealthCareAB_v1.Controllers;
 
+[ExcludeFromCodeCoverage]
 [ApiController]
 [Route("api/[controller]")]
 [Authorize(Roles = Roles.Caregiver)]
@@ -21,17 +24,36 @@ public class CaregiverController(ICaregiverService caregiverService) : Controlle
     [HttpGet("GetUpcomingSchedule")]
     public async Task<IActionResult> GetUpcomingScheduleAsync()
     {
-        var caregiverId = GetUserId();
-
-        if (caregiverId == null)
+        try
         {
-            return Unauthorized();
+            var caregiverId = GetUserId();
+
+            if (caregiverId == null)
+            {
+                return Unauthorized();
+            }
+
+            var upcomingScheduleOverview = await _caregiverService.GetUpcomingSchedulesAsync(
+                Guid.Parse(caregiverId)
+            );
+
+            return Ok(upcomingScheduleOverview);
         }
-
-        var upcomingScheduleOverview = await _caregiverService.GetUpcomingSchedulesAsync(
-            Guid.Parse(caregiverId)
-        );
-
-        return Ok(upcomingScheduleOverview);
+        catch (UnauthorizedAccessException ex)
+        {
+            return StatusCode(403, new { message = ex.Message });
+        }
+        catch (NotFoundException ex)
+        {
+            return NotFound(new { message = ex.Message });
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(new { message = ex.Message });
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(400, new { message = ex.Message });
+        }
     }
 }
