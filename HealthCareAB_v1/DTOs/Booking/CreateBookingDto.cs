94665ccefa8297a2f95300f079b3684b9b90175d
Martin <saadv89@gmail.com>
2026-01-20 15:07:41 +0100

    Removed property duplicated CaregiverDailyScheduleId

--- HealthCareAB_v1/DTOs/Booking/CreateBookingDto.cs
+++ HealthCareAB_v1/DTOs/Booking/CreateBookingDto.cs
@@ -5,7 +5,6 @@ public class CreateBookingDto
     public string Comment { get; set; } = string.Empty;
     public TimeOnly Start { get; set; }
     public DateOnly Date { get; set; }
-    public Guid CaregiverDailyScheduleId { get; set; }
     public Guid TimeSlotId { get; set; }
     public Guid ScheduleId { get; set; }
 }
