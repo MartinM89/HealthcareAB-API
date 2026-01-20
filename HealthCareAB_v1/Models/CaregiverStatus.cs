496578c7c92d59b1052ef1676566bf6b141c3c2e
Martin <saadv89@gmail.com>
2026-01-20 15:07:53 +0100

    Status is not empty string as default

--- HealthCareAB_v1/Models/CaregiverStatus.cs
+++ HealthCareAB_v1/Models/CaregiverStatus.cs
@@ -3,7 +3,7 @@ namespace HealthCareAB_v1.Models;
 public class CaregiverStatus
 {
     public Guid Id { get; set; }
-    public string Status { get; set; } = "UNAVAILABLE";
+    public string Status { get; set; } = string.Empty;
 
     public ICollection<CaregiverDailySchedule> CaregiverDailySchedules { get; set; } = [];
 }
